d96d32d75a912159fb234d5b60ab47c5e7ae9edc
Mateusz Wisniewski <matiwisnia16@gmail.com>
2024-02-28 20:31:40 +0100

    castling

---
+++ ChessLogic/Moves/Castle.cs
@@ -0,0 +1,65 @@
+﻿namespace ChessLogic
+{
+    public class Castle : Move
+    {
+        public override MoveType Type { get; }
+        public override Position FromPosition { get; }
+        public override Position ToPosition { get; }
+
+        private readonly Direction kingMoveDirection;
+        private readonly Position rookFromPosition;
+        private readonly Position rookToPosition;
+
+        public Castle(MoveType type, Position kingPosition)
+        {
+            Type = type;
+            FromPosition = kingPosition;
+
+            if (type == MoveType.CastleKS)
+            {
+                kingMoveDirection = Direction.East;
+                ToPosition = new Position(kingPosition.Row, 6);
+                rookFromPosition = new Position(kingPosition.Row, 7);
+                rookToPosition = new Position(kingPosition.Row, 5);
+            }
+            else if (type == MoveType.CastleQS)
+            {
+                kingMoveDirection = Direction.West;
+                ToPosition = new Position(kingPosition.Row, 2);
+                rookFromPosition = new Position(kingPosition.Row, 0);
+                rookToPosition = new Position(kingPosition.Row, 3);
+            }
+        }
+
+        public override void Execute(Board board)
+        {
+            new NormalMove(FromPosition, ToPosition).Execute(board);
+            new NormalMove(rookFromPosition, rookToPosition).Execute(board);
+        }
+
+        public override bool IsLegal(Board board)
+        {
+            Player player = board[FromPosition].Color;
+
+            if (board.IsInCheck(player))
+            {
+                return false;
+            }
+
+            Board boardCopy = board.Copy();
+            Position kingPosInCopy = FromPosition;
+
+            for (int i = 0; i < 2; i++)
+            {
+                new NormalMove(kingPosInCopy, kingPosInCopy + kingMoveDirection).Execute(boardCopy);
+                kingPosInCopy += kingMoveDirection;
+
+                if (boardCopy.IsInCheck(player)) {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
--- ChessLogic/Pieces/King.cs
+++ ChessLogic/Pieces/King.cs
@@ -22,6 +22,49 @@
             Color = color;
         }
 
+        private static bool IsUnmovedRook(Position position, Board board)
+        {
+            if (board.IsEmpty(position))
+            {
+                return false;
+            }
+
+            Piece piece = board[position];
+
+            return piece.Type == PieceType.Rook && !piece.HasMoved;
+        }
+
+        private static bool AllEmpty(IEnumerable<Position> positions, Board board)
+        {
+            return positions.All(pos => board.IsEmpty(pos));
+        }
+
+        private bool CanCastleKingSide(Position fromPosition, Board board)
+        {
+            if (HasMoved)
+            {
+                return false;
+            }
+
+            Position rookPos = new Position(fromPosition.Row, 7);
+            Position[] betweenPositions = new Position[] { new(fromPosition.Row, 5), new(fromPosition.Row, 6) };
+
+            return IsUnmovedRook(rookPos, board) && AllEmpty(betweenPositions, board);
+        }
+
+        private bool CanCastleQueenSide(Position fromPosition, Board board)
+        {
+            if (HasMoved)
+            {
+                return false;
+            }
+
+            Position rookPos = new Position(fromPosition.Row, 0);
+            Position[] betweenPositions = new Position[] { new(fromPosition.Row, 1), new(fromPosition.Row, 2), new(fromPosition.Row, 3) };
+
+            return IsUnmovedRook(rookPos, board) && AllEmpty(betweenPositions, board);
+        }
+
         public override Piece Copy()
         {
             King copy = new King(Color);
@@ -53,6 +96,25 @@
             {
                 yield return new NormalMove(fromPosition, to);
             }
+
+            if (CanCastleKingSide(fromPosition, board))
+            {
+                yield return new Castle(MoveType.CastleKS, fromPosition);
+            }
+
+            if (CanCastleQueenSide(fromPosition, board))
+            {
+                yield return new Castle(MoveType.CastleQS, fromPosition);
+            }
+        }
+
+        public override bool CanCaptureOpponentKing(Position fromPosition, Board board)
+        {
+            return MovePositions(fromPosition, board).Any(to =>
+            {
+                Piece piece = board[to];
+                return piece != null && piece.Type == PieceType.King;
+            });
         }
     }
 }
